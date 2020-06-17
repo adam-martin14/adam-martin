f8f3ea7fcce6aefbe1a1f301733b6432fba5d229
Adam Martin <>
2020-06-17 15:48:16 -0700

    Header mobile and desktop layout finalised. SASS implmented for header CSS styles.

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,42 +1,73 @@
 import React from 'react';
 import classes from './MainHeader.css';
-import adamPic from '../../assets/images/me-bw.jpg';
+import adamPic from '../../assets/images/me-bw-cropped.jpg';
 import linkedinLogo from '../../assets/icons/linkedin-logo.jpg';
 import codepenLogo from '../../assets/icons/codepen-logo.png';
 import locationPin from '../../assets/icons/location-pin.png';
 
 const mainHeader = (props) => (
-    <div className={classes.MainHeader}>
+    <div className={classes.HeaderContainer}>
 
-        <div className={classes.MainHeaderContent}>
+            <div className={classes.HeaderContentContainer}>
 
-            <div className={classes.MainHeaderCTA}>
-                <div>
-                    <h1>Hi, I'm Adam</h1>
-                    <h2>I'm a Web Developer based in California.</h2>
-                </div>
-                <img className={classes.AdamImage} src={adamPic}></img>
-            </div>
+                    <div className={classes.MobileHeaderIntroContainer}>
+                        <h1>Hi, I'm Adam</h1>
+                        <h2>I'm a Web Developer based in California.</h2>
+                    </div>
+
+                    <div className={classes.HeaderImageContainer}>
+                        <img className={classes.AdamImage} src={adamPic}></img>
+                    </div>
+
+                    <div className={classes.MobileHeaderLocationContainer}>
+                        <img className={classes.LocationPinImage} src={locationPin}></img>
+                        <h3>Sacramento, CA</h3>
+                    </div>
+                    
+
+                    {/* MAKE OWN COMPONENT */}
+                    <div className={classes.MobileHeaderIconStrip}>
+                        <a href="https://www.linkedin.com/in/adam-martin14/" target="_blank">
+                            <img className={classes.IconImage} src={linkedinLogo}></img>
+                        </a>
+
+                        <a href="https://codepen.io/a-martin" target="_blank">
+                            <img className={classes.IconImage} src={codepenLogo}></img>
+                        </a>
+                    </div>
 
 
-            <div className={classes.MainHeaderLocation}>
-                <img className={classes.LocationPinImage} src={locationPin}></img>
-                <h3>Sacramento, CA</h3>
-            </div>
 
 
-            {/* MAKE OWN COMPONENT */}
-            <div className={classes.HeaderIconStrip}>
-                <a href="https://www.linkedin.com/in/adam-martin14/" target="_blank">
-                    <img className={classes.IconImage} src={linkedinLogo}></img>
-                </a>
+                    <div className={classes.DesktopHeaderIntroLocationIconsContainer}>
 
-                <a href="https://codepen.io/a-martin" target="_blank">
-                    <img className={classes.IconImage} src={codepenLogo}></img>
-                </a>
-            </div>
-            
-        </div>
+                        <div className={classes.DesktopHeaderIntroContainer}>
+                            <h1>Hi, I'm Adam</h1>
+                            <h2>I'm a Web Developer based in California.</h2>
+                        </div>
+
+                        <div className={classes.DesktopLocationIconsContainer}>
+
+                            <div className={classes.DesktopHeaderLocationContainer}>
+                                <img className={classes.LocationPinImage} src={locationPin}></img>
+                                <h3>Sacramento, CA</h3>
+                            </div>
+                            
+                            {/* MAKE OWN COMPONENT */}
+                            <div className={classes.DesktopHeaderIconStrip}>
+                                <a href="https://www.linkedin.com/in/adam-martin14/" target="_blank">
+                                    <img className={classes.IconImage} src={linkedinLogo}></img>
+                                </a>
+
+                                <a href="https://codepen.io/a-martin" target="_blank">
+                                    <img className={classes.IconImage} src={codepenLogo}></img>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+
+            </div>         
                 
     </div>
 );
