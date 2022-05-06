90651e616df08e57c8f2885cb33ba9fcebdbacf4
Adam Martin <>
2022-05-05 17:27:40 -0700

    Copy changes.

--- src/components/CardGrid/CardGrid.js
+++ src/components/CardGrid/CardGrid.js
@@ -14,14 +14,14 @@ const cardGrid = (props) => (
             <div className={classes.Card}>
 
                 <h2>Front End Web Development</h2>
-                <p>Using clean, maintainable <strong>HTML, CSS</strong> (including the <strong>BEM</strong> methodology and preprocessors such as <strong>SASS</strong>), <strong>JavaScript</strong>, and libraries such as <strong>jQuery</strong> and <strong>Bootstrap</strong>, I build attractive user interfaces, that comply with designs, wireframes, and client needs. I also have an understanding of modern JavaScript frameworks such as <strong>Vue and ReactJS</strong>. My structured, component-based approach to web development reduces code reuse and enhances project maintainability.</p>
+                <p>Using clean, maintainable <strong>HTML, CSS</strong> (including the <strong>BEM</strong> methodology and preprocessors such as <strong>SASS</strong>), <strong>JavaScript</strong>, and libraries such as <strong>jQuery</strong> and <strong>Bootstrap</strong>, I build attractive user interfaces, that comply with designs, wireframes, and client needs. My structured, component-based approach to web development reduces code reuse and enhances project maintainability.</p>
 
             </div>
 
             <div className={classes.Card}>
 
                 <h2>Content Management System Web Development</h2>
-                <p>My substantial experience building and maintaining <strong>Wordpress</strong> sites has rendered me skilled at developing page templates using <strong>PHP</strong> (and PHP templating engines such as <strong>Twig and Blade</strong>), and selectively utilizing plugins such as <strong>Advanced Custom Fields</strong> to add CMS/ website functionality. In my most recent role, I gained an introductory understanding of the <strong>Drupal</strong> CMS, and used this to perform various maintenance and support tasks on existing client websites.</p>
+                <p>My substantial experience building and maintaining <strong>Wordpress</strong> sites has rendered me skilled at developing page templates using <strong>PHP</strong> (and PHP templating engines such as <strong>Twig and Blade</strong>), and selectively utilizing plugins such as <strong>Advanced Custom Fields</strong> to add CMS/ website functionality. In a previous role, I gained an introductory understanding of the <strong>Drupal</strong> CMS, and used this to perform various maintenance and support tasks on existing client websites.</p>
             </div>
 
             <div className={classes.Card}>
